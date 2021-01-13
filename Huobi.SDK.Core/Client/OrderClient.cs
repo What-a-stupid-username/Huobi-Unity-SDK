e19dbf813d08e20fb34c3b98f9f33ce8147b5859
hjk lab <568246876@qq.com>
2021-01-13 17:27:54 +0800

    add order manager

--- Huobi.SDK.Core/Client/OrderClient.cs
+++ Huobi.SDK.Core/Client/OrderClient.cs
@@ -4,6 +4,8 @@ using Huobi.SDK.Model.Response.Order;
 using Huobi.SDK.Model.Request.Order;
 using Newtonsoft.Json;
 using System;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace Huobi.SDK.Core.Client
 {
@@ -319,6 +321,188 @@ namespace Huobi.SDK.Core.Client
                 var res = task.Result;
                 action(res.data, res.code, res.message);
             });
+        }
+    }
+
+    public class OrderManager
+    {
+        [System.Serializable]
+        public struct Order
+        {
+            public string symbol;
+            public enum Type { buy, sell };
+
+            public Type type;
+
+            public float price;
+
+            public float amount;
+
+            private long id;
+            private OrderManager orders;
+
+            public Order(OrderManager orders, long id)
+            {
+                this.orders = orders;
+                this.id = id;
+                symbol = "";
+                type = Type.buy;
+                price = -1;
+                amount = -1;
+            }
+
+            public void Cancel()
+            {
+                orders.CancelOrder(id);
+            }
+        }
+
+        public OrderClient orderClient;
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        public int orderUpdatePeriod;
+
+        List<Order> orders_ = new List<Order>();
+        public Order[] orders { get { return orders_.ToArray(); } }
+
+        string accountId = "";
+
+        /// <summary>
+        /// Cancel Order Async
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="action">failed callback, call when failed.</param>
+        public void CancelOrder(long id, System.Action<string> failed = null)
+        {
+            orderClient.CancelOrderByIdAsync(id.ToString(), (a, b, c) => { if (b != null) failed?.Invoke(b); });
+        }
+
+        /// <summary>
+        /// Place Order Async
+        /// </summary>
+        /// <param name="symbol">trade symbol</param>
+        /// <param name="type">type</param>
+        /// <param name="amount">buy/sell amout, when use market price, this is amout to sell, or amout used to by</param>
+        /// <param name="price">price, -1 to use market price</param>
+        /// <param name="retryNum">retry if failed</param>
+        /// <param name="callback">order if suc</param>
+        public void PlaceOrder(string symbol, Order.Type type, float amount, float price = -1, int retryNum = 0, System.Action<System.Nullable<Order>> callback = null)
+        {
+            if (accountId != "")
+            {
+                var request = new Huobi.SDK.Model.Request.Order.PlaceOrderRequest()
+                {
+                    AccountId = accountId,
+                    amount = amount.ToString(),
+                    source = "spot-api",
+                    price = price < 0 ? "0" : price.ToString(),
+                    symbol = symbol,
+                    type = type.ToString() + (price < 0 ? "-market" : "-limit")
+                };
+
+                System.Action<int> task = null;
+                task = (remain) =>
+                {
+                    orderClient.PlaceOrderAsync(request,
+                        (id, ec, em) =>
+                        {
+                            if (ec == null && em == null)
+                            {
+                                Order order = new Order(this, long.Parse(id))
+                                {
+                                    amount = amount,
+                                    price = price,
+                                    symbol = symbol,
+                                    type = type,
+                                };
+
+                                callback?.Invoke(order);
+                            }
+                            else if (remain-- > 0)
+                            {
+                                Console.WriteLine(ec + "\n" + em + "\nretry: " + remain);
+                                task(remain);
+                            }
+                            else
+                            {
+                                Console.WriteLine(ec + "\n" + em);
+                                callback?.Invoke(null);
+                            }
+                        });
+                };
+                task(retryNum);
+            }
+            else
+            {
+                Console.WriteLine("connection is invalid, try later");
+                callback?.Invoke(null);
+            }
+        }
+
+        public void PlaceOrder(Order order, int retryNum = 0, System.Action<System.Nullable<Order>> callback = null)
+        {
+            PlaceOrder(order.symbol, order.type, order.amount, order.price, retryNum, callback);
+        }
+
+        async void UpdateOrders()
+        {
+            CancellationToken token = tokenSource.Token;
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (token.IsCancellationRequested) return;
+
+                    orderClient.GetOpenOrdersAsync(new Huobi.SDK.Core.GetRequest(), (orders, errorCode, errorMsg) => {
+
+                        List<Order> orders__ = new List<Order>();
+                        foreach (var order in orders)
+                        {
+                            var o = new Order(this, order.id)
+                            {
+                                amount = float.Parse(order.amount),
+                                price = float.Parse(order.price),
+                                symbol = order.symbol
+                            };
+                            o.type = order.type.Contains("buy") ? Order.Type.buy : Order.Type.sell;
+
+                            orders__.Add(o);
+                        }
+                        orders_ = orders__;
+                    });
+
+                    Thread.Sleep(orderUpdatePeriod);
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            tokenSource.Cancel();
+        }
+
+        public OrderManager(string acc, string sec, int orderUpdatePeriod = 1000)
+        {
+            this.orderUpdatePeriod = orderUpdatePeriod;
+            HttpRequest.httpClient.Timeout = System.TimeSpan.FromSeconds(5);
+            orderClient = new OrderClient(acc, sec);
+
+            System.Action GetAcount = null;
+            GetAcount = () =>
+            {
+                AccountClient accountClient = new AccountClient(acc, sec);
+                accountClient.GetAccountInfoAsync((account, status) => {
+                    if (status != "ok") GetAcount();
+                    else
+                    {
+                        accountId = account[0].id.ToString();
+                        Console.WriteLine("Order server connection finish.");
+                    }
+                });
+            };
+            GetAcount();
+
+            UpdateOrders();
         }
     }
 }
